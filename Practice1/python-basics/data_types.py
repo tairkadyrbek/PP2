1cdbccbbc46ce55707f89e943acd7b15a561994c
Tair Kadyrbek <tair@192.168.0.13>
2026-01-27 19:59:55 +0500

    Data Types

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,27 @@
+#1
+x = "Hello"
+print(type(x)) #str
+
+#2
+x = 23
+print(type(x)) #int
+
+#3
+x = 12.32
+print(type(x)) #float
+
+#4
+x = ["mango", "dragonfruit", "blueburry"]
+print(type(x)) #list
+
+#5
+x = ("apple", "banana", "cherry")
+print(type(x)) #tuple
+
+#6
+x = {"name" : "Jack", "age" : 18}
+print(type(x)) #dict
+
+#7
+x = False
+print(type(x)) #bool
