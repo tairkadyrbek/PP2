66cc80f5a62792b73da8bd69c7f02b60136dd78a
Tair Kadyrbek <tair@192.168.0.13>
2026-01-27 19:51:12 +0500

    Comments

---
+++ Practice1/python-basics/comments.py
@@ -0,0 +1,15 @@
+#This is comment
+
+print("Hello!") #This is a comment
+
+#print("Python")
+
+"""
+This is a comment 
+written in 
+more than just one line
+"""
+
+#This is a comment
+#written in
+#more than just one line
