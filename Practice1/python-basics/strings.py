82f9afc7dc9f0eabf7c68498e7fb0976322816d3
Tair Kadyrbek <tair@192.168.0.13>
2026-01-27 20:27:59 +0500

    Strings

---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,33 @@
+#1
+print("World")
+
+#2
+print("It's pleasure to meet you!")
+print('The sign said, "No Entry."')
+
+#3
+a = "Well"
+print(a)
+
+#4
+a = """This is a sentence 
+written in 
+more than just one line"""
+print(a)
+
+#5
+a = "Hello, World!"
+print(a[5]) # answer = ,
+
+#6
+for x in "apple":
+    print(x)
+    
+#7
+a = "Hello, World!"
+print(len(a))
+
+#8
+txt = "Python programming is fun."
+print("is" in txt)
+
