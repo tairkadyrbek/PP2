518a2fc2a3673b5fb5f6fe3029f380e5e00b4423
Tair Kadyrbek <tair@192.168.0.13>
2026-01-27 19:50:32 +0500

    Variables

---
+++ Practice1/python-basics/variables.py
@@ -0,0 +1,24 @@
+#1
+x = 18
+y = "Tair"
+print(x)
+print(y)
+
+#2
+x = 2
+x = "Hello"
+print(x)
+
+#3
+z = float(3) # z will be 3.0
+
+#4
+car_name = "Porsche"
+
+#5
+x = "awesome"
+
+def func():
+    print("Python is " + x)
+    
+func()
