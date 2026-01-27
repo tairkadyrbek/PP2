8ec269c539e72b9e003e541f37895a91cf0e6c6b
Tair Kadyrbek <tair@192.168.0.13>
2026-01-27 19:32:26 +0500

    Syntax

---
+++ Practice1/python-basics/syntax.py
@@ -0,0 +1,14 @@
+#1
+if 13 > 9:
+    print("Thirteen is greater than nine!")
+
+#2
+if 5 > 2:
+     print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+
+#3
+print("Hello!")
+print("Good afternoon!")
+print("Learning Python is fun!")
