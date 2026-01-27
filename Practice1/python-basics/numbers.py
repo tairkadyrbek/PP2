85fe94c9d7923601314b4969348b68dc217cc353
Tair Kadyrbek <tair@192.168.0.13>
2026-01-27 20:14:12 +0500

    Numbers

---
+++ Practice1/python-basics/numbers.py
@@ -0,0 +1,16 @@
+x = 324242 #int
+y = 4.52 # float
+z = -2j #complex
+
+#convert from int to float
+a = float(x)
+
+#float->int
+b = int(y)
+
+print(a)
+print(b)
+
+#Random Number
+import random
+print(random.randrange(1, 100))
