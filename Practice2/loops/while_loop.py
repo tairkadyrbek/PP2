14790a9e8edac9bee1295a306c269d0907356e1b
Tair Kadyrbek <tair@192.168.0.4>
2026-02-03 22:16:19 +0500

    while loop

---
+++ Practice2/loops/while_loop.py
@@ -0,0 +1,31 @@
+#1
+i = 1
+while i < 9:
+    print(i)
+    i += 1
+
+#2
+i = 1
+s = 0
+while i <= 10:
+    s += i
+    i += 1
+print("Sum = ", s)
+
+#3
+i = 1
+while i <= 10:
+    print(i, i*i)
+    i += 1
+    
+#4
+i = 10
+while i > 0:
+    print(i)
+    i -= 1
+    
+#5
+i = 0
+while i < 5:
+    print("Hello")
+    i += 1
