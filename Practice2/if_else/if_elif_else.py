3351745df1bea032b241517adc2adea36aa8408a
Tair Kadyrbek <tair@192.168.0.4>
2026-02-03 22:04:38 +0500

    if, elif, else statements

---
+++ Practice2/if_else/if_elif_else.py
@@ -0,0 +1,51 @@
+#1
+a = 11
+b = 11
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+
+#2
+a = 192
+b = 45
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("a is greater than b")
+
+#3
+temp = 24
+if temp > 30:
+    print("It's hot outside")
+elif temp > 20:
+    print("It's warm outside")
+elif temp > 10:
+    print("It's cool outside")
+else:
+    print("It's cold outside")
+    
+#4
+score = 70
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+
+#5
+age = 25
+if age < 13:
+    print("You are a child")
+elif age < 20:
+    print("You are a teenager")
+elif age < 65:
+    print("You are an adult")
+else:
+    print("You are a senior")
