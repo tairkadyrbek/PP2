038d5921fe1c9ef324012be3201f6712b12be8fc
Tair Kadyrbek <tair@192.168.0.4>
2026-02-03 21:36:57 +0500

    boolean operators

---
+++ Practice2/boolean/boolean_operators.py
@@ -0,0 +1,23 @@
+#1 AND Operator
+battery = 10
+charger_connected = False
+print(battery > 20 and charger_connected)    # False
+
+#2 OR Operator
+is_admin = False
+is_owner = False
+print(is_admin or is_owner)   # True
+
+#3 NOT Operator
+game_over = False
+print(not game_over)    # True
+
+#4
+age = 19
+has_id = True
+print(age >= 18 and (has_id or age >= 21))    # True
+
+#5
+username_correct = True
+password_correct = True
+print(username_correct and password_correct)  # True
