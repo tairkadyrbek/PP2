2e9534fda3338c6220ec6f59f8ea812c161f4e2c
Tair Kadyrbek <tair@192.168.0.4>
2026-02-03 21:25:13 +0500

    boolean comparison

--- Practice2/boolean/boolean_comparison.py
+++ Practice2/boolean/boolean_comparison.py
@@ -1,10 +1,24 @@
-a = 3
-b = 15
+#1 Comparing
+a = 23
+b = 17
+print(a > b)    # True
 
-# print(a > b)   # False
-# print(a < b)   # True
-# print(a == b)  # False
-# print(a != b)  # True
+#2 Equality
+c = 13
+d = 13
+print(c == d)   # True
 
-# print("rocket" == "rocket") # True
-print(5 == "5")
+#3
+x = 52
+y = 43
+print(x != y)   # True
+
+#4
+score = 68
+passed = score >= 70
+print(passed)   # True
+
+#5
+city1 = "Almaty"
+city2 = "Taraz"
+print(city1 == city2)   # False
