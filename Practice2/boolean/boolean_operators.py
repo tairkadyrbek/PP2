c079cf7ff7fd76ef781e2bb3efacfd312b66a9f1
Tair Kadyrbek <tair@192.168.0.4>
2026-02-03 21:39:52 +0500

    boolean operators

--- Practice2/boolean/boolean_operators.py
+++ Practice2/boolean/boolean_operators.py
@@ -1,14 +1,14 @@
-#1 AND Operator
+#1 Logical Operators
 battery = 10
 charger_connected = False
 print(battery > 20 and charger_connected)    # False
 
-#2 OR Operator
+#2
 is_admin = False
 is_owner = False
 print(is_admin or is_owner)   # True
 
-#3 NOT Operator
+#3
 game_over = False
 print(not game_over)    # True
 
