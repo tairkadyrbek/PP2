599858084e8f4f1cf572c730026282c2f03dd940
Tair Kadyrbek <tair@192.168.0.4>
2026-02-03 21:54:53 +0500

    if else statements

---
+++ Practice2/if_else/if_else.py
@@ -0,0 +1,35 @@
+#1
+a = 53
+b = 123
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")
+
+#2
+number = 21
+if number % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
+    
+#3
+username = "Ryan"
+if len(username) > 0:
+    print(f"Welcome, {username}!")
+else:
+    print("Error: Username cannot be empty")
+
+#4
+age = 15
+if age >= 18:
+    print("Access granted")
+else:
+    print("Access denied")
+
+#5
+temp = 35
+if temp > 25:
+    print("It's hot outside")
+else:
+    print("It's not hot outside")
