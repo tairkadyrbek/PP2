13482f27f4583e5fc6f77ecf46b03a3e9822ab45
Tair Kadyrbek <tair@192.168.0.4>
2026-02-03 21:46:49 +0500

    if statement

---
+++ Practice2/if_else/if_statement.py
@@ -0,0 +1,28 @@
+#1 
+a = 53
+b = 123
+if b > a:
+    print("b is greater than a")
+
+#2
+number = 42
+if number > 0:
+    print("The number is positive")
+    
+#3
+age = 18
+if age >= 18:
+    print("You are an adult")
+    print("You can vote")
+    print("You have full legal rights")
+    
+#4
+is_logged_in = True
+if is_logged_in:
+    print("Welcome back!")
+    
+#5
+has_homework = False
+if not has_homework:
+    print("No homework today")
+
