2f7288cb45c297414609e35c93abc371b1b2a72d
Tair Kadyrbek <tair@192.168.0.4>
2026-02-03 21:15:38 +0500

    boolean intro

--- Practice2/boolean/boolean_intro.py
+++ Practice2/boolean/boolean_intro.py
@@ -1,7 +1,9 @@
+#1
 print(23 > 17)
 print(23 == 17)
 print(23 < 17)
 
+#2
 a = 37
 b = 21
 
@@ -9,3 +11,19 @@ if a < b:
     print("b is greater than a.")
 else:
     print("b is not greater than a")
+
+#3 
+print(bool("Python"))
+print(bool(15))
+
+#4
+x = "World"
+y = 26
+
+print(bool(x))
+print(bool(y))
+
+#5
+bool("def")
+bool(456)
+bool(["apple", "cherry", "banana"])
